355675da77453115fc99e391cd5161718fc35534
GermanVelandia <german.velandia@swissaxis.ch>
2022-10-05 15:50:25 -0500

    Ahora la aplicaion espera asta que haya tweets

--- start.js
+++ start.js
@@ -1,7 +1,7 @@
 import mysql from "mysql";
 import { Builder, By } from "selenium-webdriver";
 const twitter = {
-    user: 'https://twitter.com/Minecraft',
+    user: 'cronicasdeckard',
     driver: null,
     data: [],
     connectionSQL: mysql.createConnection({
@@ -13,56 +13,59 @@ const twitter = {
     executeSQL (sql) {
         twitter.connectionSQL.connect(function(err) {
             if (err) throw err;
-            console.log("Connected!");
             twitter.connectionSQL.query(sql, function (err, result, fields) {
               if (err) throw err;
               return result;
             });
         });
     },
-    /*newDatabase () {
-        twitter.executeSQL("CREATE DATABASE twitter");
-        twitter.executeSQL(`CREATE TABLE tweet (
-            id  int(11) NOT NULL AUTO_INCREMENT,
-            nom varchar(50) DEFAULT NULL,
-            user varchar(50) DEFAULT NULL,
-            text varchar(280) DEFAULT NULL,
-            PRIMARY KEY (id)
-          ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;`);
-    },*/
     getTweet: async (e) => {
-        let text = await e.getText();
-        console.log();
+        let text = await e.findElement(By.css("div[data-testid=tweetText]")).getText();
         return text.replace(/(\r\n|\n|\r|')/gm, " ");
     },
-    getUserNames: async () => {
-        let userNamer = await twitter.driver.findElement(By.css("div[data-testid=User-Names]")).getText();
-        return userNamer.split("\n").splice(0, 2);
+    getUserTweet: async (e) => {
+        let userTweet = await e.findElement(By.css("div[data-testid=User-Names]")).getText();
+        userTweet = userTweet.split("\n")
+        userTweet[2] = userTweet[3]
+        userTweet.pop()
+        return userTweet;
+    },
+    setData: async (result) => {
+        for (let e of result) twitter.data.push({
+            tweet: await twitter.getTweet(e),
+            ussernames: await twitter.getUserTweet(e),
+        })
     },
     saveTweets: async () => {
-        let sql = `INSERT INTO tweet (nom, user, text) VALUES `;
+        let insert = `INSERT INTO tweet (nom, user, text, date) VALUES `;
         let values = '';
-        for (let data of twitter.data) values += ((values != '') ? ',' : '') + ` ('${data.ussernames[0]}', '${data.ussernames[1]}', '${data.tweet}')`;
-        twitter.executeSQL(sql + values);
-        console.log( twitter.data)
+        for (let data of twitter.data) values += ((values != '') ? ',' : '') + ` ('${data.ussernames[0]}', '${data.ussernames[1]}', '${data.tweet}'), '${data.ussernames[2]}'`;
+        twitter.executeSQL(insert + values);
     },
     start: async () => {
+        var result = []
+        var loadTwitts = false
+        var tries = 0
+
         twitter.driver = await new Builder().forBrowser('chrome').build();
-        try {
-            await twitter.driver.get(twitter.user);
-            await twitter.driver.sleep(5000);
-            await twitter.driver.executeScript("window.scrollTo(0, document.body.scrollHeight);");
-            let result = await twitter.driver.findElements(By.css("div[data-testid=tweetText]"));
-            for (let e of result) {
-                twitter.data.push({
-                    tweet: await twitter.getTweet(e),
-                    ussernames: await twitter.getUserNames(e),
-                })
-            } twitter.saveTweets()
+        await twitter.driver.get('https://twitter.com/'+twitter.user);
+        
+        while(!loadTwitts){
+            result = await twitter.driver.findElements(By.css("div[class='css-1dbjc4n r-1iusvr4 r-16y2uox r-1777fci r-kzbkwu']"));
+            loadTwitts = result.length > 0 || tries  > 20
+            await new Promise(resolve => setTimeout(resolve, 1000))
+            tries ++
         }
-        finally {
-            await twitter.driver.quit();
-        }
+            
+        if (tries < 20){
+            await twitter.setData(result); 
+            console.log(twitter.data);
+            //twitter.saveTweets()
+        } else console.log('Error en la red de twitter')
+        
+        console.log(`Llegaron ${twitter.data.length} twitts`)
+        await twitter.driver.quit();
+    
     }
 }
 twitter.start()
