0e45c4a24fe92e18a79917be87160bd13b378a30
GermanVelandia <german.velandia@swissaxis.ch>
2022-10-05 09:34:04 -0500

    se agregó node-fetch para hacer peticiones POST

--- start.js
+++ start.js
@@ -1,18 +1,21 @@
+import fetch from "node-fetch";
 const { webdriver, Builder, By, Key } = require('selenium-webdriver');
 const twitter = {
     driver: null,
-    getUserName: async () => await twitter.driver.findElement(By.css("div[data-testid=User-Names]")).getText(),
-    getTweet: async (e) => await e.findElement(By.css("div[data-testid=tweetText]")).getText(),
-    saveTweet(tweet = null, ussername = null) {
-        const formData = new FormData();
-        formData.append('tweet', tweet);
-        formData.append('ussername', ussername);
+    data: [],
+    getUserNames: async () => {
+        let userNamer = await twitter.driver.findElement(By.css("div[data-testid=User-Names]")).getText();
+        return userNamer.split("\n").splice(0, 2);
+    },
+    getTweet: async (e) => await e.getText(),
+    saveTweets (tweet = null, ussername = null) {
+        console.log(twitter.data)
         fetch('https://example.com/profile', {
-            method: 'POST', // or 'PUT'
+            method: 'POST', 
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify(formData),
+            body: JSON.stringify({}),
         })
     },
     start: async () => {
@@ -21,13 +24,13 @@ const twitter = {
             await twitter.driver.get("https://twitter.com/getbootstrap");
             await twitter.driver.sleep(5000);
             await twitter.driver.executeScript("window.scrollTo(0, document.body.scrollHeight);");
-            let result = await twitter.driver.findElements(By.css("div[data-testid=cellInnerDiv]"));
+            let result = await twitter.driver.findElements(By.css("div[data-testid=tweetText]"));
             for (let e of result) {
-                twitter.saveTweet(
-                    await twitter.getTweet(e),
-                    await twitter.getUserName()
-                )
-            }
+                twitter.data.push({
+                    tweet: await twitter.getTweet(e),
+                    ussernames: await twitter.getUserNames(e),
+                })
+            } twitter.saveTweets()
         }
         finally {
             await twitter.driver.quit();
