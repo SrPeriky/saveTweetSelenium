f42652c1b3646b82d988c9dd89f09e3d7f93e5a0
GermanVelandia <german.velandia@swissaxis.ch>
2022-10-07 14:25:28 -0500

    se agregó metodo para cerrar conexion a la base de datos

--- start.js
+++ start.js
@@ -1,27 +1,37 @@
 import mysql from "mysql";
 import { Builder, By } from "selenium-webdriver";
 const db = {
-    connectionSQL: mysql.createConnection({
-        host: "localhost",
-        user: "root",
-        password: "",
-        database: "twitter",
-    }),
+    connection: null,
+    on: function () { 
+        this.connection = mysql.createConnection({
+            host: "localhost",
+            user: "root",
+            password: "",
+            database: "twitter",
+        })
+    },
     executeSQL (sql) {
-        db.connectionSQL.connect(function(err) {
+        this.connection.connect(function(err) {
             if (err) throw err;
-            db.connectionSQL.query(sql, function (err, result, fields) {
+            this.connection.query(sql, function (err, result, fields) {
               if (err) throw err;
               return result;
             });
         });
     },
+    off: function () {
+        if(this.connection != null){
+            this.connection.end()
+            this.connection = null
+        }
+    },
 }
+
 const twitter = {
     driver: null,
     data: [],
     start: async function (user) {
-        var result = []
+        var elements = []
         var loadTwitts = false
         var tries = 0
 
@@ -30,44 +40,45 @@ const twitter = {
         await this.driver.get('https://twitter.com/'+user);
         
         while(!loadTwitts){
-            result = await this.driver.findElements(By.css("[data-testid='tweet']"));
-            loadTwitts = result.length > 0 || tries  > 20
+            elements = await this.driver.findElements(By.css("[data-testid='tweet']"));
+            loadTwitts = elements.length > 0 || tries  > 20
             await new Promise(resolve => setTimeout(resolve, 1000))
             tries ++
         }
             
         if (tries < 20){
-            await this.setData(result); 
+            await this.setData(elements)
             await this.saveTweets()
         } else console.log('Error en la red de twitter')
-        console.log(`Llegaron ${this.data.length} twitts`)
-        
+                
         this.end()
-        return 0;
     },
-    getTweet: async function (e) {
+    getTextTweet: async function (e) {
         let text = await e.findElement(By.css("[data-testid=tweetText]")).getText();
         return text.replace(/(\r\n|\n|\r|')/gm, " ");
     },
-    getUserTweet: async function (e) {
-        let userTweet = await e.findElement(By.css("[data-testid=User-Names]")).getText();
-        userTweet = userTweet.split("\n")
-        return {nom: userTweet[0], user: userTweet[1], date: userTweet[3]};
+    getDataTweet: async function (e) {
+        let dataTweet = await e.findElement(By.css("[data-testid=User-Names]")).getText();
+        dataTweet = dataTweet.split("\n")
+        return {nom: dataTweet[0], user: dataTweet[1], date: dataTweet[3]};
     },
     setData: async function (result) {
         for (let e of result) this.data.push({
-            tweet: await this.getTweet(e),
-            usertweet: await this.getUserTweet(e),
+            tweet: await this.getTextTweet(e),
+            dataTweet: await this.getDataTweet(e),
         })
     },
     saveTweets: async function () {
+        db.on()
         let insert = `INSERT INTO tweet (nom, user, text, date) VALUES `;
         let values = '';
-        for (let value of this.data) values += ((values != '') ? ',' : '') + ` ('${value.usertweet.nom}', '${value.usertweet.user}', '${value.tweet}', '${value.usertweet.date}')`;
+        for (let value of this.data) values += ((values != '') ? ',' : '') + ` ('${value.dataTweet.nom}', '${value.dataTweet.user}', '${value.tweet}', '${value.dataTweet.date}')`;
         db.executeSQL(insert + values);
+        db.off()
     },
     end: async function (){
+        console.log(`Llegaron ${this.data.length} twitts`)
         await this.driver.quit() 
     }
 }
-twitter.start('e_bueso')
+twitter.start('PirryOficial')
