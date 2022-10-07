9675827e401438bbe431a61af443584848bb52f0
GermanVelandia <german.velandia@swissaxis.ch>
2022-10-07 14:56:58 -0500

    nombres más especificos para las variebles

--- start.js
+++ start.js
@@ -1,5 +1,5 @@
-import mysql from "mysql";
-import { Builder, By } from "selenium-webdriver";
+import mysql from "mysql"
+import { Builder, By } from "selenium-webdriver"
 
 const db = {
     connection: mysql.createConnection({
@@ -10,12 +10,12 @@ const db = {
     }),
     executeSQL: (sql) => {
         db.connection.connect(function(err) {
-            if (err) throw err;
+            if (err) throw err
             db.connection.query(sql, function (err, result, fields) {
-              if (err) throw err;
-              return result;
-            });
-        });
+              if (err) throw err
+              return result
+            })
+        })
     },
     off: () => {
         if(db.connection != null){
@@ -27,53 +27,55 @@ const db = {
 
 const twitter = {
     driver: null,
-    data: [],
+    tweets: [],
     start: async function (user) {
-        var elements = []
-        var loadTwitts = false
-        var tries = 0
+        let elements = []
+        let loadTwitts = false
+        let tries = 0
 
-        this.driver = await new Builder().forBrowser('chrome').build();
+        this.driver = await new Builder().forBrowser('chrome').build()
         
-        await this.driver.get('https://twitter.com/'+user);
+        await this.driver.get('https://twitter.com/'+user)
         
         while(!loadTwitts){
-            elements = await this.driver.findElements(By.css("[data-testid='tweet']"));
+            elements = await this.driver.findElements(By.css("[data-testid='tweet']"))
             loadTwitts = elements.length > 0 || tries  > 20
             await new Promise(resolve => setTimeout(resolve, 1000))
             tries ++
         }
             
         if (tries < 20){
-            await this.setData(elements)
+            await this.setTweets(elements)
             await this.saveTweets()
         } else console.log('Error en la red de twitter')
                 
         this.end()
     },
     getTextTweet: async function (e) {
-        let text = await e.findElement(By.css("[data-testid=tweetText]")).getText();
-        return text.replace(/(\r\n|\n|\r|')/gm, " ");
+        let text = await e.findElement(By.css("[data-testid=tweetText]")).getText()
+        return text.replace(/(\r\n|\n|\r|')/gm, " ")
     },
     getDataTweet: async function (e) {
-        let dataTweet = await e.findElement(By.css("[data-testid=User-Names]")).getText();
+        let dataTweet = await e.findElement(By.css("[data-testid=User-Names]")).getText()
         dataTweet = dataTweet.split("\n")
-        return {nom: dataTweet[0], user: dataTweet[1], date: dataTweet[3]};
+        return {nom: dataTweet[0], user: dataTweet[1], date: dataTweet[3]}
     },
-    setData: async function (result) {
-        for (let e of result) this.data.push({
-            tweet: await this.getTextTweet(e),
+    setTweets: async function (result) {
+        for (let e of result) this.tweets.push({
+            textTweet: await this.getTextTweet(e),
             dataTweet: await this.getDataTweet(e),
         })
     },
     saveTweets: async function () {
-        let insert = `INSERT INTO tweet (nom, user, text, date) VALUES `;
-        let values = '';
-        for (let value of this.data) values += ((values != '') ? ',' : '') + ` ('${value.dataTweet.nom}', '${value.dataTweet.user}', '${value.tweet}', '${value.dataTweet.date}')`;
-        db.executeSQL(insert + values);
+        let insert = `INSERT INTO tweet (nom, user, text, date) VALUES `
+        let values = ''
+        for (let tw of this.tweets) {
+            values += ((values != '') ? ',' : '') + 
+            ` ('${tw.dataTweet.nom}', '${tw.dataTweet.user}', '${tw.textTweet}', '${tw.dataTweet.date}')`
+        } db.executeSQL(insert + values)
     },
     end: async function (){
-        console.log(`Llegaron ${this.data.length} twitts`)
+        console.log(`Llegaron ${this.tweets.length} tweets`)
         await this.driver.quit() 
         db.off()
     },
